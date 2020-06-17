050dd344eef1ade3a81aa555c28cf9bcc9a1daee
Eloagbawe <eloagbawe@gmail.com>
2020-06-17 23:07:21 +0100

    feat: implement patch profile API endpoint

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-import { getProfile, deleteProfile } from '../controllers/profile.controller';
+import { getProfile, deleteProfile, updateProfile } from '../controllers/profile.controller';
 
 //Handle get request for a single user profile details
 router.get('/api/v1/profile/:id', getProfile);
 router.delete('/api/v1/profile/:id', deleteProfile);
+router.patch('/api/v1/profile/:id', updateProfile);
 
 
 export default router;
