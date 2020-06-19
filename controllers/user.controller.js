2a84bb6494baf2ef30dfc837d26f071e5c06ca6c
Alao-Abiodun <abiodunDev@gmail.com>
2020-06-19 14:48:37 +0100

    fix/refractor the codebase

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,90 +4,98 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const signUp = (req, res) => {
-    const { firstName, lastName, email, username, password, isAdmin } = req.body;
+class UserController {
 
-    if (!firstName || !lastName || !email || !username || !password || !isAdmin) {
-        return res.status(200).json({ message: 'Please enter all fields' });
-    }
+    signUp(req, res) {
+        const { firstName, lastName, email, username, password, isAdmin } = req.body;
 
-    User.findOne({ email })
-        .then(user => {
-            if (user) return res.json({ status: 400, message: 'User already exists' })
+        if (!firstName || !lastName || !email || !username || !password || !isAdmin) {
+            return res.status(200).json({ message: 'Please enter all fields' });
+        }
 
-            const newUser = new User({
-                firstName,
-                lastName,
-                email,
-                username,
-                password,
-                isAdmin
-            });
+        User.findOne({ email })
+            .then(user => {
+                if (user) return res.json({ status: 400, message: 'User already exists' })
 
-            bcrypt.genSalt(10, (err, salts) => {
-                bcrypt.hash(newUser.password, salts, (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash;
-                    newUser.save()
-                        .then(user => {
+                const newUser = new User({
+                    firstName,
+                    lastName,
+                    email,
+                    username,
+                    password,
+                    isAdmin
+                });
 
-                            jwt.sign(
-                                { id: user.id },
-                                process.env.JWT_SECRET,
-                                { expiresIn: 3600 },
-                                (err, token) => {
-                                    if (err) throw err;
-                                    res.json({
-                                        status: 200,
-                                        data: {
-                                            token,
-                                            id: newUser.id,
-                                            username: newUser.username,
-                                            email: newUser.email,
-                                            password: newUser.password
-                                        }
-                                    })
-                                }
-                            )
-                        })
+                bcrypt.genSalt(10, (err, salts) => {
+                    bcrypt.hash(newUser.password, salts, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => {
+
+                                jwt.sign(
+                                    { id: user.id },
+                                    process.env.JWT_SECRET,
+                                    { expiresIn: 3600 },
+                                    (err, token) => {
+                                        if (err) throw err;
+                                        res.json({
+                                            status: 200,
+                                            data: {
+                                                token,
+                                                id: newUser.id,
+                                                username: newUser.username,
+                                                email: newUser.email,
+                                                password: newUser.password
+                                            }
+                                        })
+                                    }
+                                )
+                            })
+                    })
                 })
             })
-        })
-}
+    }
 
+    signIn(req, res) {
+        const signIn = (req, res, next) => {
+            const { email, password } = req.body;
 
-const signIn = (req, res, next) => {
-    const { email, password } = req.body;
-
-    User.findOne({ email })
-        .then(user => {
-            if (!user) {
-                return res.json({ status: 404, message: 'User not found, please provide valid credentials' });
-            }
-
-            bcrypt.compare(password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.json({ status: 403, message: 'Incorrect username or password, please review details and try again' });
+            User.findOne({ email })
+                .then(user => {
+                    if (!user) {
+                        return res.json({ status: 404, message: 'User not found, please provide valid credentials' });
                     }
-                    const token = jwt.sign(
-                        { email: user.email, id: user.id },
-                        process.env.JWT_SECRET,
-                        { expiresIn: 3600 }
-                    );
 
-                    res.json({
-                        status: 200,
-                        data: {
-                            id: user.id,
-                            token,
-                            message: 'User Logged in Sucessfully'
-                        }
-                    });
-                });
-        })
-        .catch(err => console.log(err));
+                    bcrypt.compare(password, user.password)
+                        .then(valid => {
+                            if (!valid) {
+                                return res.json({ status: 403, message: 'Incorrect username or password, please review details and try again' });
+                            }
+                            const token = jwt.sign(
+                                { email: user.email, id: user.id },
+                                process.env.JWT_SECRET,
+                                { expiresIn: 3600 }
+                            );
+
+                            res.json({
+                                status: 200,
+                                data: {
+                                    id: user.id,
+                                    token,
+                                    message: 'User Logged in Sucessfully'
+                                }
+                            });
+                        });
+                })
+                .catch(err => console.log(err));
+        }
+    }
+
 }
 
-export { signUp, signIn, profile };
+
+
+
+export default new UserController();
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,8 +5,6 @@ import { signUp, signIn } from '../controllers/user.controller';
 
 router.post('/api/v1/auth/signup', signUp);
 router.post('/api/v1/auth/signin', signIn);
-//Handle get request for a single user profile details
-router.get('/api/v1/profile/:id', profile);
 
 export default router;
 
