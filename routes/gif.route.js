77d2a7732a408f084f442ce9d15a7ea235a64338
Alao-Abiodun <abiodunDev@gmail.com>
2020-06-23 16:11:34 +0100

    feat/create a DELETE Endpoint for gif-model

--- routes/gif.route.js
+++ routes/gif.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import gifCtrl from '../controllers/git.controller';
 
 router.post('/gif', gifCtrl.createGif);
+router.delete('/:id', gifCtrl.deleteGif);
 
 export default router;
